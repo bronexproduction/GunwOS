7192bfd553d6ea87e50687952947f1c7c48ca8e8
Artur Danielewski <artur.danielewski@gmail.com>
2022-06-20 16:52:56 +0200

    kernel.c

---
+++ src/system/src/kernel/kernel.s
@@ -0,0 +1,19 @@
+;
+;  kernel.s
+;  GunwOS
+;
+;  Created by Artur Danielewski on 17.01.2018.
+;
+
+; This entry point is put at the beginning of the kernel by the linker
+; If removed, there will be no call __kernel_start at the beginning of the kernel binary
+; even though kernel.o contains such symbol
+;
+; entry point here must be at KERNEL_MEM_OFFSET, as defined in ../shared/data.s
+
+BITS 32
+
+EXTERN __kernel_start
+
+    call __kernel_start
+    jmp $
