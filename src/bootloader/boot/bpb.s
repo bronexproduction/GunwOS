7c3babe3e37d016f16e1aa710526050360e24762
Artur Danielewski <artur.danielewski@gmail.com>
2022-06-28 22:01:26 +0200

    bootloader - reserved 80 bytes for BPB expansion

--- src/bootloader/boot/bpb.s
+++ src/bootloader/boot/bpb.s
@@ -11,6 +11,20 @@
 
 BITS 16
 
+    ; --------------------------------------- 
+    ; BIOS parameter block start offset
+    ; --------------------------------------- 
+BPB_START_OFFSET                    equ 0xB
+    ; --------------------------------------- 
+    ; BIOS parameter block byte limit
+    ;
+    ; The largest BPB size so far
+    ; is used by FAT32 file system (79 bytes)
+    ; so the limit is set
+    ; to exceed that value just a bit
+    ; --------------------------------------- 
+BPB_BYTE_LIMIT                      equ 80
+
     ; ---------------------------------------
     ; FAT reserved sectors
     ; BIOS parameter block
@@ -28,3 +42,10 @@ bpb_root_directory_entries:         dw 224
 bpb_total_logical_sectors:          dw 2880
 bpb_media_descriptor:               db 0xF0
 bpb_logical_sectors_per_fat:        dw 9
+
+    ; --------------------------------------- 
+    ; Bytes reserved
+    ; for further BPB expansion
+    ; up to BPB_BYTE_LIMIT
+    ; --------------------------------------- 
+bpb_safety_fill:                    times (BPB_START_OFFSET+BPB_BYTE_LIMIT)-($-$$) db 0
