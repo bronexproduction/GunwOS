106bf14bdeaa5a71b30c489856323163c1435805
Artur Danielewski <artur.danielewski@gmail.com>
2022-07-06 22:18:42 +0200

    changed output filename for qemu

--- scripts/qemu_run.sh
+++ scripts/qemu_run.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-qemu-system-i386 -soundhw pcspk -drive file=build/system.img,index=0,if=floppy,format=raw
+qemu-system-i386 -monitor stdio -soundhw pcspk -drive file=build/gunwos.img,index=0,if=floppy,format=raw
