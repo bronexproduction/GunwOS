40078cdfb22bff6614a15caa53a113ce58cc5e92
Artur Danielewski <artur.danielewski@gmail.com>
2022-07-18 21:02:06 +0200

    test stubs

---
+++ tests/bootloader/boot/run.cpp
@@ -0,0 +1,11 @@
+//
+//  run.cpp
+//  GunwOS Tests
+//
+//  Created by Artur Danielewski on 18.07.2022.
+//
+
+int main() {
+
+    return 0;
+}
---
+++ tests/bootloader/boot/scenario/scenario1.cpp
@@ -0,0 +1,6 @@
+//
+//  scenario1.cpp
+//  GunwOS Tests
+//
+//  Created by Artur Danielewski on 18.07.2022.
+//
