c417d1ef08922d60c7b4aa1ca99b30a466590c2c
Artur Danielewski <artur.danielewski@gmail.com>
2022-08-22 11:59:07 +0200

    comments

--- src/bootloader/preloader/preloader.s
+++ src/bootloader/preloader/preloader.s
@@ -8,11 +8,11 @@
 
 ORG 0x10000
 
-%include "data.s"
-
 BITS 16
 
 jmp preloader_start
+
+%include "data.s"
 
 BITS 16
 
@@ -21,12 +21,31 @@ preloader_start:
     mov bx, MSG_PRELOADER_START
     call print_str_16
 
+    ; ---------------------------------------
+    ; Try to activate A20 line
+    ; ---------------------------------------
 %include "a20/a20_16.s"
 
+    ; ---------------------------------------
+    ; Load kernel binary
+    ;
+    ; NOTE:
+    ; Binary is expected to fit
+    ; in 0x20000 - 0x9FFFF address range
+    ; (starting from 0x2000:0x0 up to 640K)
+    ; Which gives 512K for kernel binary
+    ; ---------------------------------------
     call read_kernel
 
+    ; ---------------------------------------
+    ; Jump to protected mode
+    ; ---------------------------------------
     jmp pmode_switch
 %include "pmode/pmode.s"
+
+    ; ---------------------------------------
+    ; Verify A20 line in protected mode
+    ; ---------------------------------------
 %include "a20/a20_32.s"
 
 BITS 32
@@ -34,7 +53,9 @@ BITS 32
     mov ebx, MSG_KERNEL_START
     call print_str_32
 
-boot_kernel:
+    ; ---------------------------------------
+    ; Start kernel
+    ; ---------------------------------------
 %include "kernel/kernel_data.s"
     mov ecx, kernel_data    ; __kernel_start will receive kernel_data pointer as parameter (fastcall)
     call KERNEL_SEG:0
