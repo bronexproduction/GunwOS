6442a216975d693ccbfeda7eff8b63ee7c880454
Artur Danielewski <artur.danielewski@gmail.com>
2022-10-04 09:03:53 +0200

    print32 - constrained to video mem

--- src/bootloader/preloader/io/print/print_32.s
+++ src/bootloader/preloader/io/print/print_32.s
@@ -8,8 +8,15 @@
 
 BITS 32
 
-VGA_MEM equ 0xb8000
-WHITE_ON_BLACK equ 0x0f
+VGA_MEM             equ 0xb8000
+WHITE_ON_BLACK      equ 0x0f
+LINES               equ 25
+COLUMNS             equ 80
+BYTES_PER_CHAR      equ 2
+VGA_MEM_END         equ VGA_MEM + (LINES * COLUMNS * BYTES_PER_CHAR)
+
+print_str_32_cur_line:
+    db 0
 
 ; Prints string (32-bit protected mode only)
 ;
@@ -27,7 +34,13 @@ print_str_32:
     je .print_str_end_32
     mov [edx], ax
     add ebx, 1
-    add edx, 2
+    add edx, BYTES_PER_CHAR
+
+    cmp edx, VGA_MEM_END
+    jl .print_str_char_32
+
+    mov edx, VGA_MEM
+
     jmp .print_str_char_32
 
 .print_str_end_32:
