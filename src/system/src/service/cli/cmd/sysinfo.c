840c81355ad37389e3d8c0ece3dcb5a47960c729
Artur Danielewski <artur.danielewski@gmail.com>
2022-12-27 19:25:35 +0100

    sysinfo command

---
+++ src/system/src/service/cli/cmd/sysinfo.c
@@ -0,0 +1,14 @@
+//
+//  sysinfo.c
+//  GunwOS
+//
+//  Created by Artur Danielewski on 27.12.2022.
+//
+
+#include "../../../../include/gunwio.h"
+#include "../cmdutil.h"
+#include "../../terminal/terminal.h"
+
+void cmd_sysinfo(const char * const params) {
+    s_trm_puts("No system info available");
+}
