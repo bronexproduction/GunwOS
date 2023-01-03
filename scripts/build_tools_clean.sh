a73ad6749c245a246e8a96a4cc5f7204082a1af0
Artur Danielewski <artur.danielewski@gmail.com>
2023-01-03 21:09:12 +0100

    build_tools_clean.sh and build tools task

---
+++ scripts/build_tools_clean.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+WORKSPACE_DIR="$PWD/gcc-workspace"
+BUILD_DIR="$WORKSPACE_DIR/gcc-build"
+MIRROR="https://ftp.mpi-inf.mpg.de/mirrors/gnu/mirror/gcc.gnu.org/pub/gcc/releases"
+VERSION=$1
+GCC_SRC_DIR="$WORKSPACE_DIR/gcc-$VERSION"
+CHECKSUM_FILE="sha512.sum"
+INSTALL_DIR="$PWD/gunwxcc-$VERSION"
+TARGET=$2
+
+set -e
+
+THREADS=$(grep -c ^processor /proc/cpuinfo)
+
+# Install dependencies
+echo "Step 1: Install dependencies"
+sudo apt update && sudo apt install build-essential m4
+
+# Create directory structure
+echo "Step 2: Creating directory structure"
+rm -rf "$WORKSPACE_DIR"
+mkdir "$WORKSPACE_DIR"
+cd "$WORKSPACE_DIR"
+
+# Download sources
+echo "Step 3: Downloading sources"
+wget "$MIRROR/gcc-$VERSION/gcc-$VERSION.tar.gz"
+wget "$MIRROR/gcc-$VERSION/$CHECKSUM_FILE"
+
+# Verify checksum
+echo "Step 4: Verifying checksum"
+CHECKSUM="$(echo $(shasum -a512 gcc-$VERSION.tar.gz) | awk '{print $1}')"
+EXPECTED_CHECKSUM="$(cat sha512.sum | grep gcc-$VERSION.tar.gz$ | awk '{print $1}')"
+
+if [ $CHECKSUM != $EXPECTED_CHECKSUM ]; then
+    echo "Invalid file checksum"
+    exit 1
+fi
+
+# Unpack sources
+echo "Step 5: Unpacking sources"
+tar -xvzf "gcc-$VERSION.tar.gz"
+
+# Download dependencies
+echo "Step 6: Downloading dependencies"
+
+# libgmp
+LIBGMP_URL=$(wget https://gmplib.org/ -q -O - | grep -o "<a href=\"https://.*tar.xz</a>" | head -n 1 | awk -F\" '{printf $2}')
+LIBGMP_FILENAME=$(grep -o gmp-.*tar.xz <<< "$LIBGMP_URL")
+LIBGMP_SRC_DIR="$WORKSPACE_DIR/$(awk -F.tar '{print $1}' <<< $LIBGMP_FILENAME)"
+LIBGMP_DIR="$GCC_SRC_DIR/gmp"
+wget "$LIBGMP_URL"
+tar -xvf "$LIBGMP_FILENAME"
+mv "$LIBGMP_SRC_DIR" "$LIBGMP_DIR"
+
+# libmpfr
+LIBMPFR_VERSION=$(wget https://www.mpfr.org/mpfr-current -q -O - | grep -o -P "(?<=<title>GNU MPFR version ).*(?=</title>)")
+LIBMPFR_SRC_DIR="$WORKSPACE_DIR/mpfr-$LIBMPFR_VERSION"
+LIBMPFR_FILENAME="mpfr-$LIBMPFR_VERSION.tar.gz"
+LIBMPFR_URL="https://www.mpfr.org/mpfr-current/$LIBMPFR_FILENAME"
+LIBMPFR_DIR="$GCC_SRC_DIR/mpfr"
+wget "$LIBMPFR_URL"
+tar -xvzf "$LIBMPFR_FILENAME"
+mv "$LIBMPFR_SRC_DIR" "$LIBMPFR_DIR"
+
+# libmpc
+LIBMPC_FILENAME=$(wget https://ftp.gnu.org/gnu/mpc/ -q -O - | grep -o "<a href=\"mpc-.*.tar.gz\"" | awk -F\" '{print $2}' | sort --version-sort | tail -n 1)
+LIBMPC_URL="https://ftp.gnu.org/gnu/mpc/$LIBMPC_FILENAME"
+LIBMPC_SRC_DIR="$WORKSPACE_DIR/$(awk -F.tar '{print $1}' <<< $LIBMPC_FILENAME)"
+LIBMPC_DIR="$GCC_SRC_DIR/mpc"
+wget "$LIBMPC_URL"
+tar -xvzf "$LIBMPC_FILENAME"
+mv "$LIBMPC_SRC_DIR" "$LIBMPC_DIR"
+
+# Create build directory
+echo "Step 7: Creating build directory"
+cd "$WORKSPACE_DIR"
+mkdir build-gcc
+cd build-gcc
+
+# Build
+echo "Step 8: Build - configuring"
+bash "../gcc-$VERSION/configure" "--prefix=$INSTALL_DIR" "--target=$TARGET" \
+        --enable-languages=c,c++ --disable-multilib --disable-nls --without-headers \
+        --with-gnu-ld
+
+echo "Step 9: Build - building"
+make "-j$THREADS"
+
+# Install
+echo "Step 10: Installing"
+mkdir $INSTALL_DIR
+make install
