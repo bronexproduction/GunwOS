e05eb37ff1ea115bb69c850ed2f906558ef31027
Artur Danielewski <artur.danielewski@gmail.com>
2023-01-04 11:07:38 +0100

    build tools update

--- scripts/build_tools_clean.sh
+++ scripts/build_tools_clean.sh
@@ -17,7 +17,7 @@ THREADS=$(grep -c ^processor /proc/cpuinfo)
 
 # Install dependencies
 echo "Step 1: Install dependencies"
-sudo apt update && sudo apt install build-essential m4
+sudo apt update && sudo apt install build-essential m4 texinfo
 
 # Create directory structure
 echo "Step 2: Creating directory structure"
@@ -44,54 +44,39 @@ fi
 echo "Step 5: Unpacking sources"
 tar -xzf "gcc-$VERSION.tar.gz"
 
-# Download dependencies
-echo "Step 6: Downloading dependencies"
+# # Download prerequisites
+echo "Step 6: Downloading prerequisites"
+cd "$GCC_SRC_DIR"
+bash contrib/download_prerequisites
+cd "$WORKSPACE_DIR"
 
-# libgmp
-LIBGMP_URL=$(wget https://gmplib.org/ -q -O - | grep -o "<a href=\"https://.*tar.xz</a>" | head -n 1 | awk -F\" '{printf $2}')
-LIBGMP_FILENAME=$(grep -o gmp-.*tar.xz <<< "$LIBGMP_URL")
-LIBGMP_SRC_DIR="$WORKSPACE_DIR/$(awk -F.tar '{print $1}' <<< $LIBGMP_FILENAME)"
-LIBGMP_DIR="$GCC_SRC_DIR/gmp"
-wget "$LIBGMP_URL"
-tar -xf "$LIBGMP_FILENAME"
-mv "$LIBGMP_SRC_DIR" "$LIBGMP_DIR"
-
-# libmpfr
-LIBMPFR_VERSION=$(wget https://www.mpfr.org/mpfr-current -q -O - | grep -o -P "(?<=<title>GNU MPFR version ).*(?=</title>)")
-LIBMPFR_SRC_DIR="$WORKSPACE_DIR/mpfr-$LIBMPFR_VERSION"
-LIBMPFR_FILENAME="mpfr-$LIBMPFR_VERSION.tar.gz"
-LIBMPFR_URL="https://www.mpfr.org/mpfr-current/$LIBMPFR_FILENAME"
-LIBMPFR_DIR="$GCC_SRC_DIR/mpfr"
-wget "$LIBMPFR_URL"
-tar -xzf "$LIBMPFR_FILENAME"
-mv "$LIBMPFR_SRC_DIR" "$LIBMPFR_DIR"
-
-# libmpc
-LIBMPC_FILENAME=$(wget https://ftp.gnu.org/gnu/mpc/ -q -O - | grep -o "<a href=\"mpc-.*.tar.gz\"" | awk -F\" '{print $2}' | sort --version-sort | tail -n 1)
-LIBMPC_URL="https://ftp.gnu.org/gnu/mpc/$LIBMPC_FILENAME"
-LIBMPC_SRC_DIR="$WORKSPACE_DIR/$(awk -F.tar '{print $1}' <<< $LIBMPC_FILENAME)"
-LIBMPC_DIR="$GCC_SRC_DIR/mpc"
-wget "$LIBMPC_URL"
-tar -xzf "$LIBMPC_FILENAME"
-mv "$LIBMPC_SRC_DIR" "$LIBMPC_DIR"
+# Download binutils
+echo "Step 7: Downloading binutils"
+BINUTILS_FILENAME="$(wget https://ftp.gnu.org/gnu/binutils/ -q -O - | grep -o -P "(?<=<a href=\").*(?=.tar.gz\">)" | sort --version-sort | tail -n 1).tar.gz"
+BINUTILS_SRC_DIR="$WORKSPACE_DIR/$(awk -F.tar '{print $1}' <<< $BINUTILS_FILENAME)"
+BINUTILS_URL="https://ftp.gnu.org/gnu/binutils/$BINUTILS_FILENAME"
+BINUTILS_DIR="$GCC_SRC_DIR/binutils"
+wget "$BINUTILS_URL"
+tar -xzf "$BINUTILS_FILENAME"
+mv "$BINUTILS_SRC_DIR" "$BINUTILS_DIR"
 
 # Create build directory
-echo "Step 7: Creating build directory"
+echo "Step 8: Creating build directory"
 cd "$WORKSPACE_DIR"
 mkdir build-gcc
 cd build-gcc
 
 # Build
-echo "Step 8: Build - configuring"
+echo "Step 9: Build - configuring"
 bash "../gcc-$VERSION/configure" "--prefix=$INSTALL_DIR" "--target=$TARGET" \
         --enable-languages=c,c++ --disable-multilib --disable-nls --without-headers \
         --with-gnu-ld
 
-echo "Step 9: Build - building"
+echo "Step 10: Build - building"
 make "-j$THREADS"
 
 # Install
-echo "Step 10: Installing"
+echo "Step 11: Installing"
 rm -rf $INSTALL_DIR
 mkdir $INSTALL_DIR
 make install
