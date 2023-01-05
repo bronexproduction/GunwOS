269818e00e2b81e7fe7097ec4938a8f28fbc26e1
Artur Danielewski <artur.danielewski@gmail.com>
2023-01-05 13:49:04 +0100

    download_tools.sh update

--- scripts/download_tools.sh
+++ scripts/download_tools.sh
@@ -5,13 +5,13 @@ set -e
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # macOS
     if [[ "$(uname -m)" == "x86_64" ]]; then
-        TOOLS_ID=
+        TOOLS_ID=1-81LfQguWHVuIF5ec_2qPr_gnGpRGwDq
     else
-        TOOLS_ID=
+        TOOLS_ID=1-81LfQguWHVuIF5ec_2qPr_gnGpRGwDq
     fi    
 else
     # Linux
-    TOOLS_ID=1lFFKgdm-P1xWkPZV1mojCOeyCxg9KSrK
+    TOOLS_ID=1-e8cyz_aMVlYcX5I7LyHgV2JNmd7Wu65
 fi
 
 QUERY=`curl -s -L "https://drive.google.com/uc?export=download&id=$TOOLS_ID" \
