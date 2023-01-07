6942e4b63c67ef8cf5ebee44f822210ff7a335a1
Artur Danielewski <artur.danielewski@gmail.com>
2023-01-08 00:23:15 +0100

    rust compiler building tweaks

--- scripts/build_rust_clean.sh
+++ scripts/build_rust_clean.sh
@@ -1,34 +1,30 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+    echo "Incorrect number of arguments"
+fi
+
 RUST_VERSION=$1
 
 TEMP_DIR="$PWD/temp"
+TOOLS_DIR="$PWD/tools"
 WORKSPACE_DIR="$TEMP_DIR/rust-workspace"
 BUILD_DIR="$WORKSPACE_DIR/rust-build"
 RUST_GIT="https://github.com/rust-lang/rust.git"
-RUST_FILENAME="$RUST_VERSION.tar.gz"
-RUST_URL="https://github.com/rust-lang/rust/archive/refs/tags/$RUST_FILENAME"
-RUST_SRC_DIR="$WORKSPACE_DIR/rust-$RUST_VERSION"
-TOOLS_DIR="$PWD/tools"
+RUST_LATEST_URL="https://github.com/rust-lang/rust/releases/latest"
 
 set -e
 set -x
 
-# Install dependencies
-echo "Step 1: Install dependencies"
-if [[ "$RUST_VERSION" == "latest" ]]; then 
-    DEPS=("git")
-    DOWNLOAD="git"
-    DOWNLOAD_PARAMS=("clone" "$RUST_GIT")
-    UNPACK="mv"
-    UNPACK_PARAMS=("rust" "rust-latest")
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    THREADS=$(sysctl -n hw.ncpu)
 else
-    DEPS=("wget")
-    DOWNLOAD="wget"
-    DOWNLOAD_PARAMS=("$RUST_URL")
-    UNPACK="tar"
-    UNPACK_PARAMS=("-xzf" "$RUST_FILENAME")
+    THREADS=$(grep -c ^processor /proc/cpuinfo)
 fi
+
+# Install dependencies
+echo "Step 1: Install dependencies" 
+DEPS=("git" "cmake" "ninja")
     
 if [[ "$OSTYPE" == "darwin"* ]]; then
     brew install ${DEPS[@]}
@@ -44,47 +40,36 @@ cd "$WORKSPACE_DIR"
 
 # Download sources
 echo "Step 3: Downloading sources"
-"$DOWNLOAD" ${DOWNLOAD_PARAMS[@]}
 
-# Unpack sources
-echo "Step 4: Unpacking sources"
-"$UNPACK" ${UNPACK_PARAMS[@]}
-
-# Create build directory
-echo "Step 5: Creating build directory"
-mkdir "$BUILD_DIR"
+if [[ "$RUST_VERSION" == "latest" ]]; then
+    latest_version_url=$(curl -Ls -o /dev/null -w %{url_effective} "$RUST_LATEST_URL")
+    RUST_VERSION=${latest_version_url##*/}
+fi
 
 INSTALL_DIR_NAME="rust-${RUST_VERSION}"
+RUST_SRC_DIR="$WORKSPACE_DIR/$INSTALL_DIR_NAME"
 INSTALL_DIR="$TOOLS_DIR/$INSTALL_DIR_NAME"
-TEMP_INSTALL_DIR="$WORKSPACE_DIR/$INSTALL_DIR_NAME"
+TEMP_INSTALL_DIR="$WORKSPACE_DIR/temp-$INSTALL_DIR_NAME"
+
+git clone --depth 1 --branch "$RUST_VERSION" "$RUST_GIT" "$RUST_SRC_DIR"
+
+# Create build directory
+echo "Step 4: Creating build directory"
+mkdir "$BUILD_DIR"
 
 # Create configuration
-echo "Step 6: Creating Rust configuration"
+echo "Step 5: Creating Rust configuration"
 cd "$BUILD_DIR"
-bash "$RUST_SRC_DIR/x" setup compiler
-
-# Build
-echo "Step 7: Building Rust compiler"
-bash "$RUST_SRC_DIR/x" build compiler
+"$RUST_SRC_DIR/configure" --prefix="$TEMP_INSTALL_DIR"
 
 # Install
-echo "Step 8: Installing Rust"
-# TODO: configure prefix
-# rm -rf $TEMP_INSTALL_DIR
-# mkdir -p $TEMP_INSTALL_DIR
-# bash "$RUST_SRC_DIR/x" install compiler
-# rm -rf "$INSTALL_DIR"
-# mkdir -p "$TOOLS_DIR"
-# mv "$TEMP_INSTALL_DIR" "$INSTALL_DIR"
-
-
-exit 4
-
-
-
-
-
+echo "Step 6: Building and installing"
+rm -rf "$TEMP_INSTALL_DIR"
+make "-j$THREADS" install
+rm -rf "$INSTALL_DIR"
+mkdir -p "$TOOLS_DIR"
+mv "$TEMP_INSTALL_DIR" "$INSTALL_DIR"
 
 # Cleanup
-echo "Step 16: Cleanup"
+echo "Step 7: Cleanup"
 rm -rf "$TEMP_DIR"
