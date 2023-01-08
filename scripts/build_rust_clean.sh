34a68eda3000b2f78927e453f2a256cfbbf1fcb3
bronexproduction <56800400+bronexproduction@users.noreply.github.com>
2023-01-08 18:32:55 +0100

    Update build_rust_clean.sh

--- scripts/build_rust_clean.sh
+++ scripts/build_rust_clean.sh
@@ -28,7 +28,7 @@ echo "Step 1: Install dependencies"
 if [[ "$OSTYPE" == "darwin"* ]]; then
     brew install git cmake ninja
 else 
-    sudo apt update && sudo apt install git
+    sudo apt update && sudo apt install git curl
 fi
 
 # Create directory structure
