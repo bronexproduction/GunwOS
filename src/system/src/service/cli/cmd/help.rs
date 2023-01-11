084a48efad83da779e1a45c26c4522660e6b555c
Artur Danielewski <artur.danielewski@gmail.com>
2023-01-11 14:07:59 +0100

    linking fix

--- src/system/src/service/cli/cmd/help.rs
+++ src/system/src/service/cli/cmd/help.rs
@@ -9,6 +9,7 @@
 
 extern "C" { pub fn s_trm_puts(szText: &str); }
 
+#[no_mangle]
 fn cmd_help(_params: &[char]) {
     unsafe {
         s_trm_puts("Command not recognized: help - did you mean \"halp\"?");
