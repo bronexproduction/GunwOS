88c097ce4adc43ac228246d4cd5af95d927aab85
Artur Danielewski <artur.danielewski@gmail.com>
2023-01-25 14:26:28 +0100

    kernel_start now calls k_proc_init

--- src/system/src/core/kernel/kernel.c
+++ src/system/src/core/kernel/kernel.c
@@ -9,6 +9,7 @@
 
 extern void k_tmr_init();
 extern void k_hal_init(const char codeSegOffset);
+extern void k_proc_init();
 extern void k_dev_init();
 extern void c_init();
 extern void k_rlp_start();
@@ -35,6 +36,7 @@ void __attribute__((fastcall, section(".start"))) __kernel_start(const struct k_
     k_init();
 
     k_hal_init(bootData->gdt_codeSegOffset);
+    k_proc_init();
     k_dev_init();
     c_init();
     k_rlp_start();
