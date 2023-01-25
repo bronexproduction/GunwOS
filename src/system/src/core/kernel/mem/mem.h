653dfb94fda3d531b214b7374cddc98cfcef4598
Artur Danielewski <artur.danielewski@gmail.com>
2023-01-25 14:24:32 +0100

    added header guard to mem.h

--- src/system/src/core/kernel/mem/mem.h
+++ src/system/src/core/kernel/mem/mem.h
@@ -5,6 +5,9 @@
 //  Created by Artur Danielewski on 27.12.2022.
 //
 
+#ifndef MEM_H
+#define MEM_H
+
 #include <stdgunw/types.h>
 
 /*
@@ -16,3 +19,5 @@ size_t s_mem_totalMemBytes();
     Returns free system memory size in bytes
 */
 size_t s_mem_freeMemBytes();
+
+#endif // MEM_H
