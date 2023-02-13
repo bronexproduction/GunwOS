1c5fb4e52086394bda7971d7ab78ff9b49328caa
Artur Danielewski <artur.danielewski@gmail.com>
2023-02-13 15:04:53 +0100

    initializing empty GDT

--- src/system/src/core/kernel/hal/gdt/gdt.c
+++ src/system/src/core/kernel/hal/gdt/gdt.c
@@ -9,7 +9,18 @@
 
 #include "gdt.h"
 
-const struct k_gdt_gdt k_gdt_gdt;
+#define GDT_CODE_EMPTY (struct k_gdt_codeEntry){ 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0 }
+#define GDT_DATA_EMPTY (struct k_gdt_dataEntry){ 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 }
+#define GDT_TSS_EMPTY  (struct k_gdt_tssEntry) { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+
+const struct k_gdt_gdt k_gdt_gdt = { 
+    0,
+    GDT_CODE_EMPTY,
+    GDT_DATA_EMPTY,
+    GDT_CODE_EMPTY,
+    GDT_DATA_EMPTY,
+    GDT_TSS_EMPTY
+};
 
 void k_gdt_loadDefault() {
 
