5655c999236c6b6bcf6df4d8e960413c7f0f1984
Artur Danielewski <artur.danielewski@gmail.com>
2023-03-01 00:04:58 +0100

    removed some comments

--- src/system/src/core/kernel/hal/proc/proc.c
+++ src/system/src/core/kernel/hal/proc/proc.c
@@ -208,37 +208,7 @@ void k_proc_switch(const size_t currentProcId, const size_t nextProcId) {
 
         // Restore next process CPU state
 
-        // From 
-        // D O     31              0         
-        // I F     ╔═══════╦═══════╗◄──┐     
-        // R       ║▒▒▒▒▒▒▒║OLD SS ║   │     
-        // E E     ╠═══════╩═══════╣ SS:ESP  
-        // C X     ║    OLD ESP    ║ FROM TSS
-        // T P     ╠═══════════════╣         
-        // I A     ║  OLD EFLAGS   ║         
-        // O N     ╠═══════╦═══════╣         
-        // N S     ║▒▒▒▒▒▒▒║OLD CS ║ NEW     
-        //   I     ╠═══════╩═══════╣ SS:ESP  
-        // │ O     ║    OLD EIP    ║   │     
-        // ▼ N     ╠═══════════════╣◄──┘     
-
         // Prepare IRET stack
-
-    	// mov ax, (4 * 8) | 3 ; ring 3 data with bottom 2 bits set for ring 3
-    	// mov ds, ax
-    	// mov es, ax 
-    	// mov fs, ax 
-    	// mov gs, ax ; SS is handled by iret
- 
-	    // ; set up the stack frame iret expects
-	    // mov eax, esp
-	    // push (4 * 8) | 3 ; data selector
-	    // push eax ; current esp
-	    // pushf ; eflags
-	    // push (3 * 8) | 3 ; code selector (ring 3 code with bottom 2 bits set for ring 3)
-	    // push test_user_function ; instruction address to return to
-	    // iret
-
         __asm__ volatile ("pushl %[mem]" : : [mem] "m" (nextCpuState->ss));
         __asm__ volatile ("pushl %[mem]" : : [mem] "m" (nextCpuState->esp));
         __asm__ volatile ("pushl %[mem]" : : [mem] "m" (nextCpuState->eflags));
