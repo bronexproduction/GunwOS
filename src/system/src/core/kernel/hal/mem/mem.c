9cff45bf13ca3d10291ef5d9400f7eb86138f4c7
Artur Danielewski <artur.danielewski@gmail.com>
2023-03-03 23:17:37 +0100

    compilation fix

--- src/system/src/core/kernel/hal/mem/mem.c
+++ src/system/src/core/kernel/hal/mem/mem.c
@@ -9,7 +9,7 @@
 #include "../proc/proc.h"
 
 struct k_mem_zone k_mem_zoneForProc(int_32 procId) {
-    enum k_gdt_dpl dpl = k_proc_getDPL(k_proc_getCurrentId());
+    enum k_gdt_dpl dpl = k_proc_getInfo(k_proc_getCurrentId()).dpl;
     (void)dpl;
 
     #warning to be implemented
