c728947123c28f511b35e09f45059e670b42a84d
Artur Danielewski <artur.danielewski@gmail.com>
2023-03-04 10:35:58 +0100

    k_mem_zoneForProc

--- src/system/src/core/kernel/hal/mem/mem.c
+++ src/system/src/core/kernel/hal/mem/mem.c
@@ -10,9 +10,21 @@
 
 struct k_mem_zone k_mem_zoneForProc(int_32 procId) {
     enum k_gdt_dpl dpl = k_proc_getInfo(k_proc_getCurrentId()).dpl;
-    (void)dpl;
+    
+    struct k_mem_zone result;
+    switch (dpl) {
+        case DPL_0:
+            result.startPtr = GDT_SEGMENT_START(r0data);
+            result.endPtr = GDT_SEGMENT_END(r0data);
+            break;
+        case DPL_3:
+            result.startPtr = GDT_SEGMENT_START(r3data);
+            result.endPtr = GDT_SEGMENT_END(r3data);
+            break;
+        default:
+            result.startPtr = 0;
+            result.endPtr = 0;
+    }
 
-    #warning to be implemented
-
-    return (struct k_mem_zone){0,0};
+    return result;
 }
