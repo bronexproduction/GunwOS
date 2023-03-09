d38e7c667706eb2e6e2f2a15ad46c865c32b252d
Artur Danielewski <artur.danielewski@gmail.com>
2023-03-09 18:03:27 +0100

    warnings

--- src/system/api/include/driver/gunwbus.h
+++ src/system/api/include/driver/gunwbus.h
@@ -19,6 +19,8 @@
 SYSCALL_DECL uint_8 rdb(uint_16 const port) {
     SYSCALL_PAR1(port);
 
+#warning TODO: check if the driver is allowed to access the requested port (driver descriptor)
+
     SYSCALL_DRIVER_FUNC(RDB);
     SYSCALL_DRIVER_INT;
     
@@ -35,6 +37,8 @@ SYSCALL_DECL uint_8 rdb(uint_16 const port) {
 SYSCALL_DECL void wrb(uint_16 const port, uint_8 const val) {
     SYSCALL_PAR1(port);
     SYSCALL_PAR2(val);
+
+#warning TODO: check if the driver is allowed to access the requested port (driver descriptor)
 
     SYSCALL_DRIVER_FUNC(WRB);
     SYSCALL_DRIVER_INT;
