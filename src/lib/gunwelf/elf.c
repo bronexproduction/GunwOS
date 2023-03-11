ae7f3536b8e301c64ffdeb4c5421c4d95219501d
Artur Danielewski <artur.danielewski@gmail.com>
2023-03-11 13:34:20 +0100

    elf library (wip)

---
+++ src/lib/gunwelf/elf.c
@@ -0,0 +1,8 @@
+//
+//  elf.c
+//  GunwOS
+//
+//  Created by Artur Danielewski on 11.03.2023.
+//
+
+#include "elf.h"
--- src/lib/stdgunw/include/char.h
+++ src/lib/stdgunw/include/char.h
@@ -5,7 +5,12 @@
 //  Created by Artur Danielewski on 15.02.2020.
 //
 
+#ifndef CHAR_H
+#define CHAR_H
+
 #include "types.h"
 
 uint_8 charisnum(const char c, uint_8 base);
 uint_8 chartonum(const char c);
+
+#define // CHAR_H
