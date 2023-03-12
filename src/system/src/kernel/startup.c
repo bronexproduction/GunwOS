f09f7a76129f9e8255ae727cc8a7bfc69ba2eb97
Artur Danielewski <artur.danielewski@gmail.com>
2023-03-12 13:49:06 +0100

    asd

--- src/system/src/kernel/startup.c
+++ src/system/src/kernel/startup.c
@@ -9,13 +9,4 @@
 
 void k_startup() {
     start("cli");
-
-    // ptr_t cliElf = (ptr_t)0x50000;
-    // const struct elfHeader32 *headerPtr = (struct elfHeader32 *)cliElf;
-
-    // (void)headerPtr;
-
-    // int i = 0;
-    // i++;
-    // #warning TO BE IMPLEMENTED
 }
