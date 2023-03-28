a0847d35dc566e16090d2b6ea5aff903f293ced3
Artur Danielewski <artur.danielewski@gmail.com>
2023-03-28 20:38:58 +0200

    even more stubs

--- src/system/kernel/driver/fat12/fat12.c
+++ src/system/kernel/driver/fat12/fat12.c
@@ -7,7 +7,14 @@
 
 #include <driver/gunwfilesys.h>
 
+static bool detect(const uint_8 * const header) {
+    return false;
+}
+
 struct gnwFileSystemDescriptor k_drv_fat12_descriptor() {
     return (struct gnwFileSystemDescriptor) {
+        /* headerOffset */ 0x0B,
+        /* headerSize */ 51,
+        /* detect */ detect
     };
 }
