e4c74cab7abdeba834aa021ad846b2fc1c9ee2b7
Artur Danielewski <artur.danielewski@gmail.com>
2023-03-28 20:11:59 +0200

    stubs

---
+++ src/system/kernel/driver/fat12/fat12.c
@@ -0,0 +1,13 @@
+//
+//  fat12.c
+//  GunwOS
+//
+//  Created by Artur Danielewski on 28.03.2023.
+//
+
+#include <driver/gunwfilesys.h>
+
+struct gnwFileSystemDescriptor k_drv_fat12_descriptor() {
+    return (struct gnwFileSystemDescriptor) {
+    };
+}
--- src/system/kernel/storage/storage.h
+++ src/system/kernel/storage/storage.h
@@ -8,4 +8,12 @@
 #ifndef STORAGE_H
 #define STORAGE_H
 
+#include <driver/gunwfilesys.h>
+
+enum k_stor_error {
+    SE_NONE = 0
+};
+
+enum k_stor_error k_stor_fileSysInstall(const struct gnwFileSystemDescriptor * const);
+
 #endif // STORAGE_H
