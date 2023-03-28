92d2e2c263e590be65b2e06f4339b29c7e03af21
Artur Danielewski <artur.danielewski@gmail.com>
2023-03-28 21:00:28 +0200

    contine

--- src/system/kernel/storage/storage.c
+++ src/system/kernel/storage/storage.c
@@ -45,6 +45,11 @@ static struct volume {
         Volume identifier/index in controller
     */
     size_t volumeId;
+    
+    /*
+        File system identifier
+    */
+    size_t fileSysId;
 } volumes[MAX_VOLUMES];
 
 static struct filesystem {
@@ -57,7 +62,7 @@ static struct filesystem {
         File system descriptor
     */
     struct gnwFileSystemDescriptor desc;
-} fileSys[MAX_VOLUMES];
+} fileSys[MAX_FILESYS];
 
 enum k_stor_error k_stor_fileSysInstall(const struct gnwFileSystemDescriptor * const desc) {
     size_t fileSysIndex = 0;
@@ -74,6 +79,78 @@ enum k_stor_error k_stor_fileSysInstall(const struct gnwFileSystemDescriptor * c
     fileSys[fileSysIndex].used = true;
     fileSys[fileSysIndex].desc = *desc;
     return SE_NONE;
+}
+
+static void addDrive(const size_t ctrlId, const size_t driveId) {
+    size_t index = 0;
+    for (; index < MAX_DRIVES; ++index) {
+        if (!drives[index].used) {
+            break;
+        }
+    }
+    if (index >= MAX_DRIVES) {
+        OOPS("Volume count limit exceeded");
+        return;
+    }
+    
+    drives[index].used = true;
+    drives[index].ctrlId = ctrlId;
+    drives[index].driveId = driveId;
+}
+
+static void detectDrives(const struct gnwDeviceUHADesc desc, bool (* const drivePresent)(uint_8)) {
+    if (!drivePresent) {
+        OOPS("Null-pointer parameter");
+        return;
+    }
+    for (size_t driveIndex = 0; driveIndex < desc.storage.driveCount; ++driveIndex) {
+        if (drivePresent(driveIndex)) {
+            addDrive(desc.identifier, driveIndex);
+        } 
+    }
+}
+
+static void detectFileSystem(const size_t driveIndex) {
+    if (driveIndex >= MAX_DRIVES) {
+        OOPS("Drive index over limit");
+        return;
+    }
+    if (!drives[driveIndex].used) {
+        OOPS("Drive descriptor empty");
+        return;
+    }
+    for (size_t fileSysIndex = 0; fileSysIndex < MAX_FILESYS; ++fileSysIndex) {
+        if (!fileSys[fileSysIndex].used) {
+            continue;
+        }
+        /*
+            Load header bytes
+        */
+        uint_8 headerBytes[51+0x0B] = {0};
+        if (fileSys[fileSysIndex].desc.detect(headerBytes)) {
+            
+            /*
+                Add volume
+            */
+            (void)volumes;
+            break;
+        }
+    }
+}
+
+static void detectVolumes() {
+    
+    /*
+        Detect volumes (filesystems)
+
+        NOTE: Restriction to single-volume drives
+    */
+    
+    for (size_t driveIndex = 0; driveIndex < MAX_DRIVES; ++driveIndex) {
+        if (drives[driveIndex].used) {
+            detectFileSystem(driveIndex);
+        }
+    }
 }
 
 void k_stor_init() {
@@ -96,50 +173,6 @@ void k_stor_init() {
         return;
     }
 
-    for (size_t driveIndex = 0; driveIndex < desc.storage.driveCount; ++driveIndex) {
-        if (uha.storage.routine.drivePresent(driveIndex)) {
-            /*
-                Add drive
-            */
-            size_t volumeIndex = 0;
-            for (; volumeIndex < MAX_DRIVES; ++volumeIndex) {
-                if (!drives[volumeIndex].used) {
-                    break;
-                }
-            }
-            if (volumeIndex >= MAX_DRIVES) {
-                OOPS("Volume count limit exceeded");
-                return;
-            }
-            drives[volumeIndex].used = true;
-            drives[volumeIndex].ctrlId = desc.identifier;
-            drives[volumeIndex].driveId = driveIndex;
-        } 
-    }
-
-    /*
-        Detect filesystems
-
-        NOTE: Restriction to single-volume drives
-    */
-    for (size_t driveIndex = 0; driveIndex < MAX_DRIVES; ++driveIndex) {
-        if (drives[driveIndex].used) {
-            for (size_t fileSysIndex = 0; fileSysIndex < MAX_FILESYS; ++fileSysIndex) {
-                if (!fileSys[fileSysIndex].used) {
-                    continue;
-                }
-                /*
-                    Load header bytes
-                */
-                uint_8 headerBytes[51+0x0B] = {0};
-                if (fileSys[fileSysIndex].desc.detect(headerBytes)) {
-                    /*
-                        Add volume
-                    */
-                    break;
-                }
-            }
-        }
-    }
-    (void)volumes;
+    detectDrives(desc, uha.storage.routine.drivePresent);
+    detectVolumes();
 }
