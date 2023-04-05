db8da89d6234e35be8bc8570464d4746080394c7
Piotr Merski <merskip@gmail.com>
2023-04-05 10:22:04 +0200

    Added CStr

--- src/system/user/gunwsh/cmd/help.rs
+++ src/system/user/gunwsh/cmd/help.rs
@@ -6,8 +6,11 @@
 //
 
 #![no_std]
+extern crate alloc;
 
-use core::ffi::{c_char, CStr};
+use core::ffi::c_char;
+use alloc::string::String;
+use alloc::vec::Vec;
 
 extern "C" {
     fn user_cli_puts(szText: *const c_char);
@@ -15,9 +18,27 @@ extern "C" {
 
 #[no_mangle]
 fn cmd_help(_params: &[char]) {
-    let message = "Command not recognized: help - did you mean \"halp\"?";
-    let message = CStr::from_bytes_with_nul(message.as_bytes()).expect("Failed to create CStr");
+    let message = CStr::from("Command not recognized: help - did you mean \"halp\"?");
     unsafe {
         user_cli_puts(message.as_ptr());
+    }
+}
+
+
+struct CStr {
+    buffer: Vec<u8>
+}
+
+impl CStr {
+    pub fn from(string: &str) -> Self {
+        let mut bytes = String::from(string).into_bytes();
+        bytes.push(0);
+        Self {
+            buffer: bytes,
+        }
+    }
+
+    pub fn as_ptr(&self) -> *const c_char {
+        self.buffer.as_ptr() as *const c_char
     }
 }
