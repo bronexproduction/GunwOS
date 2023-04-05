ed375903c7ec2aa3683e8aebf573636f1a16131b
Piotr Merski <merskip@gmail.com>
2023-04-05 10:28:39 +0200

    Force set \0 on the end of string

--- src/system/user/gunwsh/cmd/help.rs
+++ src/system/user/gunwsh/cmd/help.rs
@@ -6,11 +6,8 @@
 //
 
 #![no_std]
-extern crate alloc;
 
 use core::ffi::c_char;
-use alloc::string::String;
-use alloc::vec::Vec;
 
 extern "C" {
     fn user_cli_puts(szText: *const c_char);
@@ -18,27 +15,8 @@ extern "C" {
 
 #[no_mangle]
 fn cmd_help(_params: &[char]) {
-    let message = CStr::from("Command not recognized: help - did you mean \"halp\"?");
+    let message = "Command not recognized: help - did you mean \"halp\"?\0";
     unsafe {
-        user_cli_puts(message.as_ptr());
-    }
-}
-
-
-struct CStr {
-    buffer: Vec<u8>
-}
-
-impl CStr {
-    pub fn from(string: &str) -> Self {
-        let mut bytes = String::from(string).into_bytes();
-        bytes.push(0);
-        Self {
-            buffer: bytes,
-        }
-    }
-
-    pub fn as_ptr(&self) -> *const c_char {
-        self.buffer.as_ptr() as *const c_char
+        user_cli_puts(message.as_ptr() as *const c_char);
     }
 }
