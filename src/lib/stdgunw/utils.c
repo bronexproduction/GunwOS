b255c82ed9a7009534be65ffd5fc94ed5f02244a
Artur Danielewski <artur.danielewski@gmail.com>
2023-04-13 13:37:37 +0200

    FIX

--- src/lib/stdgunw/utils.c
+++ src/lib/stdgunw/utils.c
@@ -9,7 +9,7 @@
 
 size_t aligned(const size_t value, const size_t alignment) {
     if (value % alignment) {
-        return value / alignment + alignment;
+        return (value / alignment) * alignment + alignment;
     }
     else {
         return value;
