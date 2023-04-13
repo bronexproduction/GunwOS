0a555b46fd7337e28c2ebf2990c4d947adb7ee26
Artur Danielewski <artur.danielewski@gmail.com>
2023-04-13 13:13:41 +0200

    update

--- src/system/kernel/startup.c
+++ src/system/kernel/startup.c
@@ -10,7 +10,7 @@
 
 void k_startup() {
     extern enum gnwCtrlError k_scr_usr_start(const char * const, const size_t);
-    enum gnwCtrlError err = k_scr_usr_start("0:/gunwsh.elf", 13);
+    enum gnwCtrlError err = k_scr_usr_start("0:/GUNWSH.ELF", 13);
     if (err != GCE_NONE) {
         OOPS("Unable to start core modules");
     }
