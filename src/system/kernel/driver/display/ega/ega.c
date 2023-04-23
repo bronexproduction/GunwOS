c544bbe8e3812017128dd254f5cde8996f3c918c
Artur Danielewski <bronex@macbook-pro-artur.home>
2023-04-23 23:44:50 +0200

    refactor in accordance to IBM documentation

--- src/system/kernel/driver/display/ega/ega.c
+++ src/system/kernel/driver/display/ega/ega.c
@@ -10,18 +10,130 @@
 #include <error/panic.h>
 #include <gunwbus.h>
 
-#define VIDEO_HW_ROWS           25
-#define VIDEO_HW_COLS           80
-#define VIDEO_BYTES_PER_CHAR    2
+static const volatile ptr_t DISPLAY_BUFFER_ADDR_BASIC = (volatile ptr_t)0xb8000;
+static const volatile ptr_t DISPLAY_BUFFER_ADDR_ENHANCED = (volatile ptr_t)0xa0000;
 
-static const volatile ptr_t EGA_VIDEO_TEXT_HW_MEM = (volatile ptr_t)0xb8000;
-static const volatile ptr_t EGA_VIDEO_GRAPHICS_HW_MEM = (volatile ptr_t)0xa0000;
+#define BUS_ADDR_MONOCHROME_MARK 0xB0
+#define BUS_ADDR_MONOCHROME_MARK_MASK 0xF0
+#define BUS_ADDR_COLOR_OFFSET 0x20
 
-#define EGA_BUS_MODE_CONTROL_REG    0x3D8
-#define EGA_BUS_COLOR_CONTROL_REG   0x3D9
+enum bus_reg_external {
+    BRE_MISC_OUT = 0x3C2,
+    BRE_FEATURE_CTRL = 0x3BA, /* MONOCHROME ADDR */
+    BRE_INPUT_STATUS_0 = 0x3C2,
+    BRE_INPUT_STATUS_1 = 0x3B2, /* MONOCHROME ADDR */
+};
 
-#define MEM_CHAR(INDEX) (EGA_VIDEO_TEXT_HW_MEM + (INDEX * VIDEO_BYTES_PER_CHAR))
+enum bus_reg_sequencer {
+    BRS_ADDRESS = 0x3C4,
+    BRS_DATA = 0x3C5
+};
+
+enum bus_reg_sequencer_index {
+    BRSI_RESET = 0x00,
+    BRSI_CLOCKING_MODE = 0x01,
+    BRSI_MAP_MASK = 0x02,
+    BRSI_CHAR_MAP_SELECT = 0x03,
+    BRSI_MEMORY_MODE = 0x04
+};
+
+enum bus_reg_crt {
+    BRC_ADDRESS = 0x3B4, /* MONOCHROME ADDR */
+    BRC_DATA = 0x3B5 /* MONOCHROME ADDR */
+};
+
+enum bus_reg_crt_index {
+    BRCI_HORIZONTAL_TOTAL = 0x00,
+    BRCI_HORIZONTAL_DISPLAY_END = 0x01,
+    BRCI_START_HORIZONTAL_BLANK = 0x02,
+    BRCI_END_HORIZONTAL_BLANK = 0x03,
+    BRCI_START_HORIZONTAL_RETRACE = 0x04,
+    BRCI_END_HORIZONTAL_RETRACE = 0x05,
+    BRCI_VERTICAL_TOTAL = 0x06,
+    BRCI_OVERFLOW = 0x07,
+    BRCI_PRESET_ROW_SCAN = 0x08,
+    BRCI_MAX_SCAN_LINE = 0x09,
+    BRCI_CURSOR_START = 0x0A,
+    BRCI_CURSOR_END = 0x0B,
+    BRCI_START_ADDRESS_HIGH = 0x0C,
+    BRCI_START_ADDRESS_LOW = 0x0D,
+    BRCI_CURSOR_LOCATION_HIGH = 0x0E,
+    BRCI_CURSOR_LOCATION_LOW = 0x0F,
+    BRCI_VERTICAL_RETRACE_START = 0x10,
+    BRCI_LIGHT_PEN_HIGH = 0x10,
+    BRCI_VERTICAL_RETRACE_END = 0x11,
+    BRCI_LIGHT_PEN_LOW = 0x11,
+    BRCI_VERTICAL_DISPLAY_END = 0x12,
+    BRCI_OFFSET = 0x13,
+    BRCI_UNDERLINE_LOCATION = 0x14,
+    BRCI_START_VERTICAL_BLANK = 0x15,
+    BRCI_END_VERTICAL_BLANK = 0x16,
+    BRCI_MODE_CONTROL = 0x17,
+    BRCI_LINE_COMPARE = 0x18
+};
+
+enum bus_reg_graphics {
+    BRG_GRAPHICS_1_POSITION = 0x3CC,
+    BRG_GRAPHICS_2_POSITION = 0x3CA,
+    BRG_GRAPHICS_1_AND_2_ADDRESS = 0x3CE,
+    BRG_DATA = 0x3CF
+};
+
+enum bus_reg_graphics_index {
+    BRGI_SET_RESET = 0x00,
+    BRGI_ENABLE_SET_RESET = 0x01,
+    BRGI_COLOR_COMPARE = 0x02,
+    BRGI_DATA_ROTATE = 0x03,
+    BRGI_READ_MAP_SELECT = 0x04,
+    BRGI_MODE_REGISTER = 0x05,
+    BRGI_MISC = 0x06,
+    BRGI_COLOR_DONT_CARE = 0x07,
+    BRGI_BIT_MASK = 0x08
+};
+
+enum bus_reg_attr {
+    BRA_ADDRESS = 0x3C0,
+    BRA_DATA = 0x3C0
+};
+
+enum bus_reg_attr_index {
+    BRAI_PALETTE_START = 0x00, /* 0x00 - 0x0F range for 16 palettes */
+    BRAI_MODE_CONTROL = 0x10,
+    BRAI_OVERSCAN_COLOR = 0x11,
+    BRAI_COLOR_PLANE_ENABLE = 0x12,
+    BRAI_HORIZONTAL_PEL_PANNING = 0x13
+};
+
+/*
+    Bus register addresses that matches 0x*B* refer to monochrome display address range.
+    Corresponding bytes for color display are offset by 0x020 (addresses 0x*D*)
+
+    For more information see:
+    IBM Enhanced Graphics Adapter
+    page 12+
+*/
+static uint_16 bus_addr(uint_16 baseAddr, enum gnwDeviceUHA_display_format format) {
+    if (!GDD_FMT_ISGRAP(format)) {
+        return baseAddr;
+    }
+
+    if ((baseAddr & BUS_ADDR_MONOCHROME_MARK_MASK) != BUS_ADDR_MONOCHROME_MARK) {
+        return baseAddr;
+    }
+    
+    return baseAddr + BUS_ADDR_COLOR_OFFSET;
+}
+
+#warning FUNCTION TO FETCH ADDRESS FOR SELECTED OPERATION MODE (color / mono differs)
+
+
+
+#define BYTES_PER_CHAR    2
+
+#define MEM_CHAR(INDEX) (DISPLAY_BUFFER_ADDR_BASIC + (INDEX * BYTES_PER_CHAR))
 #define MEM_COLOR(INDEX) (MEM_CHAR(INDEX) + 1)
+
+static point_t dimensions = { -1, -1 };
 
 // enum k_drv_display_ega_modeRegBit {
 //     /*
@@ -165,15 +277,18 @@ point_t uhaDimensionsForFormat(const enum gnwDeviceUHA_display_format format) {
 bool setFormat(const enum gnwDeviceUHA_display_format format) {
     uint_8 mode;
 
-    wrb(EGA_BUS_MODE_CONTROL_REG, 0);
+    (void)mode;
+    (void)bus_addr;
+
+    // wrb(EGA_BUS_MODE_CONTROL_REG, 0);
 
     switch(format) {
     case TEXT_H80V25C16:
     #warning TEST       
-        mode = rdb(0x3cf);
-        wrb(0x3cf, mode | 1);
-        mode = rdb(0x3c0);
-        wrb(0x3c0, mode | 1);
+        // mode = rdb(0x3cf);
+        // wrb(0x3cf, mode | 1);
+        // mode = rdb(0x3c0);
+        // wrb(0x3c0, mode | 1);
         // mode = EGA_MRB_VOUT_ENABLE | EGA_MRB_GRAPHICS;
         break;
     case GRAPHICS_H320V200C16:
@@ -183,7 +298,9 @@ bool setFormat(const enum gnwDeviceUHA_display_format format) {
         return false;
     }
 
-    wrb(EGA_BUS_MODE_CONTROL_REG, mode);
+    dimensions = uhaDimensionsForFormat(format);
+
+    // wrb(EGA_BUS_MODE_CONTROL_REG, mode);
 
     return true;
 }
@@ -196,7 +313,7 @@ static void update(const void * const buffer, const range_addr_t inputBufferRang
 
     const struct gnwDeviceUHA_display_character * const charBuffer = (struct gnwDeviceUHA_display_character *)buffer;
 
-    for (int index = 0; index < VIDEO_HW_COLS * VIDEO_HW_ROWS; ++index) {
+    for (int index = 0; index < dimensions.x * dimensions.y; ++index) {
         *MEM_CHAR(index) = charBuffer[index].character;
         *MEM_COLOR(index) = (charBuffer[index].charColor | charBuffer[index].bgColor << 4);
     }
@@ -217,7 +334,7 @@ static struct gnwDeviceUHA uha() {
     uha.system.routine.getParam = uhaGetParam_display;
     uha.system.routine.setParam = uhaSetParam_display;
     uha.display.desc.supportedFormatCount = 2;
-    uha.mem.desc.bytesRange.offset = (addr_t)EGA_VIDEO_GRAPHICS_HW_MEM;
+    uha.mem.desc.bytesRange.offset = (addr_t)DISPLAY_BUFFER_ADDR_ENHANCED;
     uha.mem.desc.bytesRange.sizeBytes = KiB(64 + 32 + 16);
     uha.mem.desc.maxInputSizeBytes = 320 * 
                                      200 * 
@@ -233,7 +350,7 @@ struct gnwDeviceDescriptor k_drv_display_ega_descriptor() {
         /* api */ uha(),
         /* driver */ (struct gnwDeviceDriver) {
             /* io */ (struct gnwDeviceIO) {
-                /* busBase */ EGA_BUS_MODE_CONTROL_REG,
+                /* busBase */ 0x3B2,
             },
             /* descriptor */ desc()
         },
@@ -251,7 +368,7 @@ struct gnwDeviceDescriptor k_drv_display_ega_descriptor() {
 //         return;
 //     }
 
-//     for (int index = 0; index < VIDEO_HW_COLS * VIDEO_HW_ROWS; ++index) {
+//     for (int index = 0; index < dimensions.x * dimensions.y; ++index) {
 //         // *MEM_CHAR(index) = buffer[index].character;
 //         // *MEM_COLOR(index) = (buffer[index].charColor | buffer[index].bgColor << 4);
 //     }
