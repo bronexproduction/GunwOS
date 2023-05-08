f80824f21b9d207ecfb570d77efe4194977c04ad
Artur Danielewski <bronex@pc-27.home>
2023-05-08 10:45:56 +0200

    added runloop cleanup to scr_exit

--- src/system/kernel/syscall/usrfunc.c
+++ src/system/kernel/syscall/usrfunc.c
@@ -98,6 +98,7 @@ SCR(exit,
     const procId_t procId = k_proc_getCurrentId();
     k_que_dispatch_arch((fPtr_arch)k_dev_procCleanup, procId);
     k_que_dispatch_arch((fPtr_arch)k_ipc_procCleanup, procId);
+    k_que_dispatch_arch((fPtr_arch)k_runloop_procCleanup, procId);
     k_proc_stop(procId);
 )
 
