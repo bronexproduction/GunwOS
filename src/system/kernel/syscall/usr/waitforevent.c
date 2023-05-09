4e7f77cf94c708f8784a73325d8502327eda0b2d
Artur Danielewski <artur.danielewski@gmail.com>
2023-05-09 09:54:39 +0200

    removed extern

--- src/system/kernel/syscall/usr/waitforevent.c
+++ src/system/kernel/syscall/usr/waitforevent.c
@@ -9,7 +9,7 @@
 #include <hal/criticalsec/criticalsec.h>
 #include <runloop/runloop.h>
 
-extern void k_scr_usr_waitForEvent(const procId_t procId) {
+void k_scr_usr_waitForEvent(const procId_t procId) {
     CRITICAL_SECTION(
         if (k_runloop_isEmpty(procId)) {
             k_proc_lockIfNeeded(procId);
