c18963b430c5f41b942651d345e8f6dbc0cbccfe
Artur Danielewski <bronex@macbook-pro-artur.home>
2023-05-25 00:00:33 +0200

    guwndisplay updates to use dispmgr

--- src/system/api/app/src/gunwdisplay.c
+++ src/system/api/app/src/gunwdisplay.c
@@ -8,156 +8,86 @@
 #ifndef _GUNWAPI_KERNEL
 
 #include "_gunwdisplay.h"
+#include <mem.h>
 #include "../include/gunwfug.h"
+#include "../include/gunwipc.h"
+#include <dispmgr.h>
 
-static bool dimensionsValid(const point_t dimensions) {
-    return dimensions.x > 0 && dimensions.y > 0;
-}
-
-static enum gnwDeviceUHA_display_format getSupportedFormat(const size_t deviceId, const size_t index) {
-    struct gnwDeviceParamDescriptor paramDesc;
-
-    paramDesc.param = GDU_DISPLAY_PARAM_FORMAT;
-    paramDesc.subParam = 0;
-    paramDesc.paramIndex = index;
-
-    enum gnwDeviceUHA_display_format format;
-
-    const enum gnwDeviceError err = devGetParam(deviceId, &paramDesc, (size_t *)&format);
-    if (err != GDE_NONE) {
-        return 0;
-    }
-
-    return format;
-}
-
-static enum gnwDeviceUHA_display_format getSupportedDisplayFormat(const enum DisplayType type,
-                                                                  const struct gnwDeviceUHADesc * const deviceUHA) {
-    for (size_t i = 0; i < deviceUHA->display.supportedFormatCount; ++i) {
-        enum gnwDeviceUHA_display_format supportedFormat = getSupportedFormat(deviceUHA->identifier, i);
-        switch (type) {
-        case TEXT:
-            if (GDD_FMT_ISTEXT(supportedFormat)) {
-                return supportedFormat;
-            }
-            break;
-        case GRAPHICS:
-            if (GDD_FMT_ISGRAP(supportedFormat)) {
-                return supportedFormat;
-            }
-            break;
-        }
-    }
-
-    return 0;
-}
-
-static point_t getDisplayDimensions(const size_t deviceId, const enum gnwDeviceUHA_display_format format) {
-    struct gnwDeviceParamDescriptor paramDesc;
-    point_t dimensions;
-
-    paramDesc.param = GDU_DISPLAY_PARAM_DIMENSIONS;
-    paramDesc.subParam = format;
-    
-    paramDesc.paramIndex = 0; {
-        const enum gnwDeviceError err = devGetParam(deviceId, &paramDesc, (size_t *)&dimensions.x);
-        if (err != GDE_NONE) {
-            return (point_t) { -1, -1 };
-        }
-    }
-
-    paramDesc.paramIndex = 1; {
-        const enum gnwDeviceError err = devGetParam(deviceId, &paramDesc, (size_t *)&dimensions.y);
-        if (err != GDE_NONE) {
-            return (point_t) { -1, -1 };
-        }
-    }
-
-    return dimensions;
-}
-
-enum gnwDeviceError setDisplayFormat(const size_t deviceId, const enum gnwDeviceUHA_display_format format) {
-    struct gnwDeviceParamDescriptor paramDesc;
-
-    paramDesc.param = GDU_DISPLAY_PARAM_FORMAT;
-    
-    return devSetParam(deviceId, &paramDesc, format);
-}
-
-void fillDisplayDescriptor(const size_t identifier,
-                           const enum gnwDeviceUHA_display_format format,
-                           const point_t dimensions,
-                           struct gnwDisplayDescriptor * const desc) {
-    CHECKPTR(desc)
-
-    desc->identifier = identifier;
-    desc->dimensions = dimensions;
-    desc->format = format;
-}
-
-enum gnwDeviceError getDisplay(const enum DisplayType type, 
+enum gnwDeviceError getDisplay(const enum gnwDisplayType type, 
                                struct gnwDisplayDescriptor * const displayDescriptor) {
     CHECKPTR(displayDescriptor)
     
-    struct gnwDeviceUHADesc deviceUHA;
-    enum gnwDeviceError e = devGetByType(DEV_TYPE_DISPLAY, &deviceUHA);
+    struct gnwDisplayManagerGetDisplayQuery query;
+    query.type = type;
+    struct gnwDisplayManagerGetDisplayResult result;
 
-    if (e) {
-        return e;
+    enum gnwIpcError error = ipcSend(DISPMGR_PATH_GET,
+                                     (ptr_t)&query, sizeof(struct gnwDisplayManagerGetDisplayQuery),
+                                     (ptr_t)&result, sizeof(struct gnwDisplayManagerGetDisplayResult));
+    if (error != GIPCE_NONE) {
+        return GDE_OPERATION_FAILED;
+    } else if (result.error != GDE_NONE) {
+        return result.error;
     }
-
-    const enum gnwDeviceUHA_display_format format = getSupportedDisplayFormat(type, &deviceUHA);
-    if (!format) {
-        return GDE_NOT_FOUND;
-    }
-    const point_t dimensions = getDisplayDimensions(deviceUHA.identifier, format);
-    if (!dimensionsValid(dimensions)) {
-        return GDE_UNKNOWN;
-    }
-
-    fillDisplayDescriptor(deviceUHA.identifier, format, dimensions, displayDescriptor);
-
+    
+    memcopy(&result.displayDescriptor, displayDescriptor, sizeof(struct gnwDisplayDescriptor));
+    displayDescriptor->dimensions.x = 80;
+    displayDescriptor->dimensions.y = 25;
+    displayDescriptor->format = TEXT_H80V25C16;
+    displayDescriptor->identifier = 0;
     return GDE_NONE;
 }
 
-enum gnwDeviceError attachToDisplay(const enum DisplayType type, 
+enum gnwDeviceError attachToDisplay(const enum gnwDisplayType type, 
                                     const struct gnwDisplayDescriptor * const displayDescriptor) {
     CHECKPTR(displayDescriptor)
     
-    struct gnwDeviceUHADesc uha;
-    enum gnwDeviceError e = devGetById(displayDescriptor->identifier, &uha);
-    
-    if (e) {
-        return e;
-    }
-    if (GDD_FMT_ISTEXT(displayDescriptor->format) != (type == TEXT)) {
-        return GDE_ID_INVALID;
-    }
+    struct gnwDisplayManagerAttachToDisplayQuery query;
+    query.type = type;
+    query.displayDescriptor = *displayDescriptor;
+    struct gnwDisplayManagerAttachToDisplayResult result;
 
-    e = devAcquire(displayDescriptor->identifier);
-    if (e) {
-        return e;
-    }
-
-    e = setDisplayFormat(displayDescriptor->identifier, displayDescriptor->format);
-    if (e) {
-        return e;
+    enum gnwIpcError error = ipcSend(DISPMGR_PATH_ATTACH,
+                                     (ptr_t)&query, sizeof(struct gnwDisplayManagerAttachToDisplayQuery),
+                                     (ptr_t)&result, sizeof(struct gnwDisplayManagerAttachToDisplayResult));
+    if (error != GIPCE_NONE) {
+        return GDE_OPERATION_FAILED;
+    } else if (result.error != GDE_NONE) {
+        return result.error;
     }
 
     return GDE_NONE;
 }
 
-enum gnwDeviceError pushFrame(const size_t deviceId,
+enum gnwDeviceError pushFrame(const size_t displayId,
                               const ptr_t buffer,
                               const size_t bufferSizeBytes) {
     CHECKPTR(buffer)
+    if (bufferSizeBytes > DISPMGR_PUSH_MAX_BYTES) {
+        return GDE_INVALID_PARAMETER;
+    }
 
     range_addr_t inputRange;
 
     inputRange.offset = 0;
     inputRange.sizeBytes = bufferSizeBytes;
 
-    return devMemWrite(deviceId, buffer, &inputRange);
+    struct gnwDisplayManagerPushFrameQuery query;
+    query.displayId = displayId;
+    memcopy(buffer, query.frameBuffer, bufferSizeBytes);
+    query.inputRange = inputRange;
+    struct gnwDisplayManagerPushFrameResult result;
+
+    enum gnwIpcError error = ipcSend(DISPMGR_PATH_PUSH,
+                                     (ptr_t)&query, sizeof(struct gnwDisplayManagerPushFrameQuery),
+                                     (ptr_t)&result, sizeof(struct gnwDisplayManagerPushFrameResult));
+    if (error != GIPCE_NONE) {
+        return GDE_OPERATION_FAILED;
+    } else if (result.error != GDE_NONE) {
+        return result.error;
+    }
+    
+    return GDE_NONE;
 }
 
 #endif // _GUNWAPI_KERNEL
