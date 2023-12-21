24d7b011aab6477610eddc94f327506fabf20a9e
Artur Danielewski <bronex@MacBook-Pro-Artur.local>
2023-12-21 14:49:22 +0100

    logs update

--- src/system/kernel/log/log.h
+++ src/system/kernel/log/log.h
@@ -13,8 +13,10 @@
 #define LOG_START LOG_NBR(__FILE__ ": ")
 #define LOG_NBR(MSG) k_log_log(MSG);
 #define LOG_END k_log_log("\n");
+#define LOG_BLOCK(LOGS) { LOG_START; { LOGS; } LOG_END; }
 
-#define LOG(MSG) { LOG_START; LOG_NBR(MSG); LOG_END; }
+#define LOG(MSG) LOG_BLOCK(LOG_NBR(MSG);)
+#define LOG2(MSG1, MSG2) LOG_BLOCK(LOG_NBR(MSG1); LOG_NBR(MSG2);)
 
 void k_log_log(const char * const absMsgPtr);
 void k_log_logd(const data_t absMsgData);
