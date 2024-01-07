177a03ce927d85ab3fef5ec5f78336e39b4b675c
Artur Danielewski <artur.danielewski@gmail.com>
2024-01-07 21:47:56 +0100

    unit tests linker script

--- scripts/qemu_run.sh
+++ scripts/qemu_run.sh
@@ -7,6 +7,8 @@ MODE=$2
 
 if [[ "$MODE" == "debug"* ]]; then
     EPARAMS=("-serial stdio" "-s" "-S" "-d cpu_reset")
+elif [[ "$MODE" == "test_unit"* ]]; then
+    EPARAMS=("-serial stdio" "-s" "-S")
 else
     EPARAMS=("-monitor stdio")
 fi
