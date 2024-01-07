2462e0cff21535e0699b6878152d8ecbd9bcb16c
Artur Danielewski <artur.danielewski@gmail.com>
2024-01-07 22:18:54 +0100

    update

--- tests/unit/modules/kernel/runner.rs
+++ tests/unit/modules/kernel/runner.rs
@@ -1,13 +1,17 @@
 #![no_std]
+#![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 #[no_mangle]
-pub fn __kernel_start() {
-    test_runner();
+pub extern "C" fn __kernel_start() -> ! {
+    // test_main();
+
+    loop {}
 }
 
-pub fn test_runner(tests: &[&dyn Fn()]) {
+fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
