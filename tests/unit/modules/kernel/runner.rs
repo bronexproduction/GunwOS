68cca83316ccc44e728785547d29bb167abe28a5
Artur Danielewski <artur.danielewski@gmail.com>
2024-01-07 22:04:22 +0100

    linker script removed

--- tests/unit/modules/kernel/runner.rs
+++ tests/unit/modules/kernel/runner.rs
@@ -2,6 +2,11 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 
+#[no_mangle]
+pub fn __kernel_start() {
+    test_runner();
+}
+
 pub fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
