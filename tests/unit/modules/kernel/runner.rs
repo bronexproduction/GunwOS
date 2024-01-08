bb4291fa1b4beccfc644e0f851a3891d38e185f5
Artur Danielewski <artur.danielewski@gmail.com>
2024-01-08 15:43:42 +0100

    runner.rs - simple messages

--- tests/unit/modules/kernel/runner.rs
+++ tests/unit/modules/kernel/runner.rs
@@ -4,9 +4,21 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+extern "C" {
+    fn k_log_log(msg: &str);
+}
+
 #[link_section = ".start_override"]
 pub extern "C" fn __kernel_start_test() -> ! {
-    test_main();
+    unsafe {
+        k_log_log("Unit tests run started\n");
+    }
+    
+    // test_main();
+
+    unsafe {
+        k_log_log("Unit tests run finished - all tests passed\n");
+    }
 
     loop {}
 }
