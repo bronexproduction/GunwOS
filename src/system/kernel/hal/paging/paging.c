389f9a292a56b1d9150cfa1414350a6fcffe3a24
Artur Danielewski <bronex@MacBook-Pro-Artur.local>
2024-02-04 20:05:46 +0100

    page tables moved to paging.c

--- src/system/kernel/hal/paging/paging.c
+++ src/system/kernel/hal/paging/paging.c
@@ -39,5 +39,41 @@ _Static_assert(MEM_PHYSICAL_PAGE_TABLE_COUNT <= MEM_MAX_DIR_ENTRY, "MEM_PHYSICAL
 
 #define CR0_PAGING_ENABLE_BIT 0x80000000
 
+typedef struct __attribute__((packed)) k_virtual_page_specifier_t {
+    bool present            :1;
+    bool writable           :1;
+    bool user               :1;
+    uint_8 _reserved1       :2;
+    bool accessed           :1;
+    bool dirty              :1;
+    uint_8 _reserved0       :2;
+    uint_8 _unused          :3;
+    uint_32 frameAddress    :20;
+} k_virtual_page_table_t[MEM_MAX_PAGE_ENTRY];
+
+struct {
+    struct __attribute__((packed)) {
+        /*
+            Lower memory addresses (TEMPORARY - to be swapped with user pages)
+        */
+        struct __attribute__((packed)) {
+            struct k_virtual_page_specifier_t kernel[MEM_VIRTUAL_RESERVED_KERNEL_PAGE_TABLE_COUNT];
+        } reserved;
+        /*
+            Topmost memory address range (TEMPORARY - to be swapped with kernel pages)
+        */
+        struct k_virtual_page_specifier_t user[MEM_VIRTUAL_USER_MAX_PAGE_TABLE_COUNT];
+    } pageDirectory;
+    
+    /*
+        CHECK ALIGNMENT
+
+        NOTE: Must be 4KiB-aligned due to the PDE addressing scheme
+    */
+    __attribute__((aligned(MEM_PAGE_SIZE_BYTES))) k_virtual_page_table_t pageTables[MEM_VIRTUAL_USER_PAGE_TABLE_COUNT];
+} pagingInfo[MAX_PROC];
+
+static __attribute__((aligned(MEM_PAGE_SIZE_BYTES))) k_virtual_page_table_t kernelPageTables[MEM_VIRTUAL_RESERVED_KERNEL_PAGE_TABLE_COUNT];
+
 void k_paging_init() {
 }
