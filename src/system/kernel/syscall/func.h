6a4248616bdcaddce1ed9517e2b8330f65bf9e01
Artur Danielewski <artur.danielewski@gmail.com>
2024-02-06 15:43:19 +0100

    project structure changes - asm calls moved to HAL

--- src/system/kernel/syscall/func.h
+++ src/system/kernel/syscall/func.h
@@ -12,13 +12,6 @@
 #include <types.h>
 #include <error/panic.h>
 
-/*
-    Syscall service routine macro
-
-    Implement functions using this macro to prevent from incidentally forgeting the return label
-*/
-#define SCR(NAME, CODE) __attribute__((naked)) void k_scr_ ## NAME () { CODE; __asm__ volatile ("ret"); }
-
 #define _SCLF_CHECK_ABS(VALUE, NULLABLE) (VALUE || !NULLABLE)
 #define _SCLF_GET_VALID_ABS_NAMED(TYPE, NAME, VALUE, SIZEBYTES, ERR_HANDLE, RETURN, NULLABLE) \
     TYPE abs_ ## NAME = _SCLF_CHECK_ABS(VALUE, NULLABLE) ? (TYPE)k_scl_func_getValidAbsoluteForProc(procId, (const ptr_t)(VALUE), SIZEBYTES) : nullptr; \
