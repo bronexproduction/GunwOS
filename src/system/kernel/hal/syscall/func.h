ea0bca7e0ff66e8bde34dfd1516cb84b700087ae
Artur Danielewski <artur.danielewski@gmail.com>
2024-02-11 13:20:56 +0100

    h

--- src/system/kernel/hal/syscall/func.h
+++ src/system/kernel/hal/syscall/func.h
@@ -8,6 +8,8 @@
 #ifndef FUNC_H
 #define FUNC_H
 
+#include "stack.h"
+
 /*
     Syscall service routine macro
 
@@ -15,14 +17,5 @@
 */
 
 #define SCR(NAME, CODE) __attribute__((naked)) void k_scr_ ## NAME () { CODE; __asm__ volatile ("ret"); }
-
-#define _STACK_REFERENCE_OFFSET 0x30
-#define _USER_STACK_PTR (ptr_t)(*((addr_t *)(kernelStack + _STACK_REFERENCE_OFFSET + 12)))
-
-#define _PAR_PTR(NAME, OFFSET) addr_t * NAME = (addr_t *)(_USER_STACK_PTR + OFFSET);
-#define PAR_PTR_1(NAME) _PAR_PTR(NAME, 4)
-#define PAR_PTR_2(NAME) _PAR_PTR(NAME, 8)
-#define PAR_PTR_3(NAME) _PAR_PTR(NAME, 12)
-
 
 #endif // FUNC_H
