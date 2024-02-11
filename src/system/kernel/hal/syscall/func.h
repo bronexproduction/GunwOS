b744d41431341fd3324617493118a18be416647e
Artur Danielewski <artur.danielewski@gmail.com>
2024-02-11 13:25:05 +0100

    ppp

--- src/system/kernel/hal/syscall/func.h
+++ src/system/kernel/hal/syscall/func.h
@@ -15,7 +15,6 @@
 
     Implement functions using this macro to prevent from incidentally forgeting the return label
 */
-
 #define SCR(NAME, CODE) __attribute__((naked)) void k_scr_ ## NAME () { CODE; __asm__ volatile ("ret"); }
 
 #endif // FUNC_H
--- src/system/kernel/hal/syscall/stack.h
+++ src/system/kernel/hal/syscall/stack.h
@@ -8,20 +8,20 @@
 #ifndef STACK_H
 #define STACK_H
 
+#include <types.h>
+
 register ptr_t kernelStack __asm__ ("esp");
 
 #define _STACK_REFERENCE_OFFSET_ENTRY 0x2C
 #define _STACK_REFERENCE_OFFSET_FUNC 0x30
 
-#define _USER_STACK_PTR_ENTRY (ptr_t)(*((addr_t *)(kernelStack + _STACK_REFERENCE_OFFSET_ENTRY + 12)))
-#define _USER_STACK_PTR_FUNC (ptr_t)(*((addr_t *)(kernelStack + _STACK_REFERENCE_OFFSET_FUNC + 12)))
+#define _USER_STACK_PTR(LOC) (ptr_t)(*((addr_t *)(kernelStack + _STACK_REFERENCE_OFFSET_ ## LOC + 12)))
 
-#define FUNC_PTR (uint_32 *)(_USER_STACK_PTR_ENTRY + 0)
+#define FUNC_CODE_PTR (addr_t *)(_USER_STACK_PTR(ENTRY) + 0)
 
-#define _PAR_PTR(NAME, OFFSET) addr_t * NAME = (addr_t *)(_USER_STACK_PTR_FUNC + OFFSET);
+#define _PAR_PTR(NAME, OFFSET) addr_t * NAME = (addr_t *)(_USER_STACK_PTR(FUNC) + OFFSET);
 #define PAR_PTR_1(NAME) _PAR_PTR(NAME, 4)
 #define PAR_PTR_2(NAME) _PAR_PTR(NAME, 8)
 #define PAR_PTR_3(NAME) _PAR_PTR(NAME, 12)
-
 
 #endif // STACK_H
