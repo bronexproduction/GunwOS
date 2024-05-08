f26f96944033e3c41ba9a78ea51a3b635edae9d3
Artur Danielewski <artur.danielewski@gmail.com>
2024-05-08 20:48:26 +0200

    memory detection stubs

---
+++ src/bootloader/preloader/boot_info/memory.s
@@ -0,0 +1,19 @@
+;
+;  memory.s
+;  GunwOS Bootloader
+;  Part of GunwOS project
+;
+;  Created by Artur Danielewski on 08.05.2024.
+;
+
+BITS 16
+
+; Detect lower memory
+
+detect_lower_memory:
+    ret
+
+; Detect upper memory
+
+detect_upper_memory:
+    ret
