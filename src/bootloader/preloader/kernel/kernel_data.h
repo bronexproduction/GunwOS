b00273605785d7804f310bd390fceb967c9d70f7
Artur Danielewski <artur.danielewski@gmail.com>
2024-05-12 19:16:25 +0200

    k_krn_emsMapEntry

--- src/bootloader/preloader/kernel/kernel_data.h
+++ src/bootloader/preloader/kernel/kernel_data.h
@@ -10,11 +10,20 @@
 
 #define E820_MAX_ENTRIES    128
 
+enum k_krn_emsMapRangeType {
+    EMRT_NONE = 0,
+    EMRT_MEMORY = 1,
+    EMRT_RESERVED = 2
+};
+_Static_assert(sizeof(enum k_krn_emsMapRangeType) == sizeof(int_32), "Unexpected enum k_krn_emsMapRangeType size");
+
 struct __attribute__((packed)) k_krn_emsMapEntry {
-    uint_64 something1;
-    uint_64 something2;
-    uint_32 something3;
-    uint_32 something4;
+    uint_32 baseAddrLow;
+    uint_32 baseAddrHigh;
+    uint_32 lengthLow;
+    uint_32 lengthHigh;
+    enum k_krn_emsMapRangeType type;
+    uint_32 _unused;
 };
 
 struct __attribute__((packed)) k_krn_bootData {
