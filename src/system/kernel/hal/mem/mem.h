e46d0bd3873a452f2248d0e17ea78b14787e7094
Artur Danielewski <artur.danielewski@gmail.com>
2024-06-02 15:27:54 +0200

    added deprecations, added k_mem_gimme, k_mem_zero, k_mem_copy stubs

--- src/system/kernel/hal/mem/mem.h
+++ src/system/kernel/hal/mem/mem.h
@@ -18,6 +18,29 @@
 
 #define MEM_XMS_START                                   MiB(1)
 
+enum k_mem_error {
+    ME_NONE = 0,
+    ME_PART_ALREADY_ASSIGNED,
+    ME_ALREADY_ASSIGNED,
+    ME_OUT_OF_MEMORY,
+    ME_UNKNOWN
+};
+
+/*
+    DEPRECATED
+
+    Returns absolute memory address 
+    of the pointer relative to process accessible space
+*/
+/*__attribute__((deprecated))*/ ptr_t k_mem_absForProc(const procId_t procId, const ptr_t relPtr);
+
+/*
+    DEPRECATED
+    
+    Checks whether the buffer is inside given process' memory zone
+*/
+/*__attribute__((deprecated))*/ bool k_mem_bufInZoneForProc(const procId_t procId, const ptr_t absPtr, const size_t bufSize);
+
 /*
     Returns linear memory address
     of the physical pointer (kernel only)
@@ -25,24 +48,36 @@
 ptr_t k_mem_physicalToLinear(const ptr_t physAddr);
 
 /*
-    Returns absolute memory address 
-    of the pointer relative to process accessible space
-*/
-ptr_t k_mem_absForProc(const procId_t procId, const ptr_t relPtr);
-
-/*
-    Checks whether the buffer is inside given process' memory zone
-*/
-bool k_mem_bufInZoneForProc(const procId_t procId, const ptr_t absPtr, const size_t bufSize);
-
-/*
-    Releases all the allocated memory for given process
-*/
-void k_mem_procCleanup(const procId_t procId);
-
-/*
     Returns the amount of free (allocable) memory (aligned to pages)
 */
 size_t k_mem_getFreeBytes();
+
+/*
+    Reserves memory for given process pointed at by vPtr
+*/
+enum k_mem_error k_mem_gimme(const procId_t procId,
+                             const ptr_t vPtr,
+                             const size_t sizeBytes);
+
+/*
+    Zeroes memory of given process pointed at by vPtr
+*/
+enum k_mem_error k_mem_zero(const procId_t procId,
+                            const ptr_t vPtr,
+                            const size_t sizeBytes);
+
+/*
+    Copies virtual memory content between processes
+*/
+enum k_mem_error k_mem_copy(const procId_t srcProcId,
+                            const ptr_t srcVPtr,
+                            const procId_t dstProcId,
+                            const ptr_t dstVPtr,
+                            const size_t sizeBytes);
+
+/*
+    Releases all the allocated and reserved memory for given process
+*/
+void k_mem_procCleanup(const procId_t procId);
 
 #endif // MEM_H
