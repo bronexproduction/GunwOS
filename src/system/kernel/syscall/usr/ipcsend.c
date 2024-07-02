8aa2fad1df427daad3e18ae321a5cb1b843308cb
Artur Danielewski <artur.danielewski@gmail.com>
2024-07-02 14:25:13 +0200

    sum logs

--- src/system/kernel/syscall/usr/ipcsend.c
+++ src/system/kernel/syscall/usr/ipcsend.c
@@ -10,6 +10,10 @@
 #include <hal/proc/proc.h>
 #include <defs.h>
 
+#include <string.h>
+#include <mem.h>
+#include <log/log.h>
+
 enum gnwIpcError k_scr_usr_ipcSend(const struct gnwIpcSenderQuery * const queryPtr) {
 
     const procId_t procId = k_proc_getCurrentId();
@@ -29,6 +33,53 @@ enum gnwIpcError k_scr_usr_ipcSend(const struct gnwIpcSenderQuery * const queryP
     absoluteQuery.replyData.ptr = abs_replyPtr;
     absoluteQuery.replyData.bytes = abs_queryPtr->replyData.bytes;
     absoluteQuery.bindData = abs_queryPtr->bindData;
+
+    {
+        char msg[31] = "k_scr_usr_ipcSend - new query ";
+        LOG(msg);
+    }
+    {
+        char msg[20] = "  sender -         ";
+        uint2hex((addr_t)procId, msg + 11);
+        LOG(msg);
+    }
+    {
+        char msg[22] = "  receiver -         ";
+        int2str((addr_t)absoluteQuery.procId, msg + 13);
+        LOG(msg);
+    }
+    {
+        char msg[128] = "  path -         ";
+        memcopy(absoluteQuery.pathData.ptr, msg + 9, absoluteQuery.pathData.bytes);
+        LOG(msg);
+    }
+    {
+        char msg[23] = "  query ptr -         ";
+        uint2hex((addr_t)queryPtr, msg + 14);
+        LOG(msg);
+    }
+    {
+        char msg[28] = "  query data ptr -         ";
+        uint2hex((addr_t)absoluteQuery.data.ptr, msg + 19);
+        LOG(msg);
+    }
+    {
+        char msg[29] = "  query data size -         ";
+        uint2hex((addr_t)absoluteQuery.data.bytes, msg + 20);
+        LOG(msg);
+    }
+    {
+        char msg[10] = "  bytes:";
+        LOG_BLOCK(
+            LOG_NBR(msg);
+            for (size_t i = 0; i < absoluteQuery.data.bytes; ++i) {
+                char byteString[3] = { 0 };
+                uint2hex((addr_t)absoluteQuery.data.ptr[i], byteString);
+                LOG_NBR(" ");
+                LOG_NBR(byteString);
+            }
+        );
+    }
 
     return k_ipc_send(procId, absoluteQuery);
 }
