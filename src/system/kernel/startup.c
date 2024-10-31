32a1f484d6b7c60eb29d2c4c2dd6f289860e698c
Artur Danielewski <artur.danielewski@gmail.com>
2024-10-31 16:36:09 +0100

    cleanup

--- src/system/kernel/startup.c
+++ src/system/kernel/startup.c
@@ -12,14 +12,14 @@
 #include <log/log.h>
 #include <error/panic.h>
 
-#define START(PATH) if (err == GCE_NONE) {                                                              \
-    LOG(PATH);                                                                                          \
-    struct gnwCtrlStartDescriptor desc;                                                                 \
-    desc.pathPtr = PATH;                                                                                \
-    desc.pathLen = strlen(desc.pathPtr);                                                                \
-    desc.errorPtr = &err;                                                                               \
-    extern void k_scr_usr_start(const procId_t procId, const struct gnwCtrlStartDescriptor * const);    \
-    k_scr_usr_start(KERNEL_PROC_ID, &desc);                                                             \
+#define START(PATH) if (err == GCE_NONE) {                                                      \
+    LOG(PATH);                                                                                  \
+    struct gnwCtrlStartDescriptor desc;                                                         \
+    desc.pathPtr = PATH;                                                                        \
+    desc.pathLen = strlen(desc.pathPtr);                                                        \
+    desc.errorPtr = &err;                                                                       \
+    extern void k_scr_usr_start(const procId_t, const struct gnwCtrlStartDescriptor * const);   \
+    k_scr_usr_start(KERNEL_PROC_ID, &desc);                                                     \
 }
 
 void k_startup() {
