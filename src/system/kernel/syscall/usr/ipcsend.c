dec1fc3921fd7b4e667090029ac01f1461ba1c49
Artur Danielewski <artur.danielewski@gmail.com>
2024-10-31 16:27:56 +0100

    cleanup

--- src/system/kernel/syscall/usr/ipcsend.c
+++ src/system/kernel/syscall/usr/ipcsend.c
@@ -55,53 +55,6 @@ enum gnwIpcError k_scr_usr_ipcSend(const procId_t procId, const struct gnwIpcSen
     if (!k_mem_bufferZoneValidForProc(procId, (ptr_t)queryPtr->replyData.ptr, queryPtr->replyData.bytes)) {
         OOPS("Reserved zone access violation", GIPCE_UNKNOWN);
     }
-
-    // {
-    //     char msg[31] = "k_scr_usr_ipcSend - new query ";
-    //     LOG(msg);
-    // }
-    // {
-    //     char msg[20] = "  sender -         ";
-    //     uint2hex((addr_t)procId, msg + 11);
-    //     LOG(msg);
-    // }
-    // {
-    //     char msg[22] = "  receiver -         ";
-    //     int2str((addr_t)queryPtr->procId, msg + 13);
-    //     LOG(msg);
-    // }
-    // {
-    //     char msg[128] = "  path -         ";
-    //     memcopy(queryPtr->pathData.ptr, msg + 9, queryPtr->pathData.bytes);
-    //     LOG(msg);
-    // }
-    // {
-    //     char msg[23] = "  query ptr -         ";
-    //     uint2hex((addr_t)queryPtr, msg + 14);
-    //     LOG(msg);
-    // }
-    // {
-    //     char msg[28] = "  query data ptr -         ";
-    //     uint2hex((addr_t)queryPtr->data.ptr, msg + 19);
-    //     LOG(msg);
-    // }
-    // {
-    //     char msg[29] = "  query data size -         ";
-    //     uint2hex((addr_t)queryPtr->data.bytes, msg + 20);
-    //     LOG(msg);
-    // }
-    // {
-    //     char msg[9] = "  bytes:";
-    //     LOG_BLOCK(
-    //         LOG_NBR(msg);
-    //         for (size_t i = 0; i < queryPtr->data.bytes; ++i) {
-    //             char byteString[3] = { 0 };
-    //             uint2hex((addr_t)queryPtr->data.ptr[i], byteString);
-    //             LOG_NBR(" ")
-    //             LOG_NBR(byteString);
-    //         }
-    //     );
-    // }
     
     return k_ipc_send(procId, *queryPtr);
 }
