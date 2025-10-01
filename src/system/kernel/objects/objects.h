045ece8d1734104b4641d1ef2246313004cc5313
Artur Danielewski <artur.danielewski@gmail.com>
2025-10-01 12:34:18 +0200

    Simple kernel objects storage implementation

---
+++ src/system/kernel/objects/objects.h
@@ -0,0 +1,36 @@
+//
+//  objects.h
+//  GunwOS
+//
+//  Created by Artur Danielewski on 01.10.2025.
+//
+
+#ifndef OBJECTS_H
+#define OBJECTS_H
+
+#include <types.h>
+
+enum k_obj_error {
+    OE_NONE = 0,
+    OE_NOT_FOUND,
+    OE_INVALID_PARAMETER,
+    OE_SIZE_LIMIT_EXCEEDED,
+    OE_FULL
+};
+
+void k_obj_init();
+
+enum k_obj_error k_obj_store(const procId_t owner,
+                             const size_t sizeBytes,
+                             const void * const data,
+                             size_t * const id);
+
+enum k_obj_error k_obj_retrieve(const procId_t owner,
+                                const size_t id,
+                                const size_t sizeBytes,
+                                data_t * const data);
+
+enum k_obj_error k_obj_remove(const procId_t owner,
+                              const size_t id);
+
+#endif // OBJECTS_H
